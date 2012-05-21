eb74cb41e9a3e2acdefd6edcd168b901681bee1b
Thomas Gillis <thomasgillis@occupywifi.westell.com>
2012-05-21 01:25:38 -0400

    title tweak

--- mosaic/mosaic.rb
+++ mosaic/mosaic.rb
@@ -1,19 +1,24 @@
 #get all of the photos within a grid size of x and y 
 class Mosaic 
-  attr_accessor :page_size, :campaign, :conditions
+  attr_accessor :page_size, :campaign, :conditions, :meta_info
   def grid(skip)
     if @campaign == nil
-      @campaign = 'natochicago'
+      @campaign = 'chicagonato'
     end
     @conditions = {
       :mayday=>{:limit=>@page_size, :skip=>skip * @page_size,:order=>:timestamp.asc, :conditions=>{'entities.media.0.media_url'=>{:$exists=>true}, 'entities.media.0.sizes.small.h'=>{:$exists=>true}, 'entities.urls.0.expanded_url'=>{'$not'=>/yfrog/}, :timestamp.gte=>1335848461, :timestamp.lte=>1335963661,:block=>{:$exists=>false}}},
-      :natochicago=>{:skip=>skip * @page_size,:limit=>30,:order=>:timestamp.desc, :conditions=>{'entities.media.0.media_url'=>{:$exists=>true},'entities.media.0.sizes.small.h'=>{:$exists=>true},:timestamp.gte=>1337302861, :timestamp.lte=>1337602332,:block=>{:$exists=>false}}}
+      :chicagonato=>{:skip=>skip * @page_size,:limit=>30,:order=>:timestamp.desc, :conditions=>{'entities.media.0.media_url'=>{:$exists=>true},'entities.media.0.sizes.small.h'=>{:$exists=>true},:timestamp.gte=>1337302861, :timestamp.lte=>1337602332,:block=>{:$exists=>false}}}
     }
+    @meta = {
+      :mayday =>{:page_title =>"Occupy Mosaic - May Day",:description=>"A mosaic of photos that were posted worldwide during the May Day General Strike"},
+      :chicagonato => {:page_title => "Occupy Mosaic - Chicago - NATO Summit", :description=>"A mosaic of photos posted on Twitter during the protests against the NATO summit in Chicago"}
+    }
+    @meta_info = @meta[@campaign.to_sym]
+    
     c = @conditions[@campaign.to_sym]
     if c == nil 
-      c = @conditions[:natochicago]
+      c = @conditions[:chicagonato]
     end
-    puts c.inspect
    CrawledTweet.all(c)
   end
 end
@@ -30,6 +35,7 @@ get '/.?:campaign?' do
   @squares = m.grid(0)
   @page = 2
   @campaign = m.campaign
+  @meta = m.meta_info
   haml 'mosaic/grid'.to_sym  
 end
 
@@ -39,6 +45,7 @@ get '/page/?:campaign/:page' do
   m.campaign = params[:campaign]
   m.page_size = 30
   @campaign = m.campaign
+  @meta = m.meta_info
   @squares =m.grid(@page)
   haml 'mosaic/grid'.to_sym  
 end
