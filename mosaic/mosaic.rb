0579c3e48957c578f9bef09dab7f1fe5dae6e2e3
root <root@work.(none)>
2012-05-31 08:22:29 +0000

    casseroles

--- mosaic/mosaic.rb
+++ mosaic/mosaic.rb
@@ -6,11 +6,13 @@ class Mosaic
       @campaign = 'chicagonato'
     end
     @conditions = {
-      :mayday=>{:limit=>@page_size, :skip=>skip * @page_size,:order=>:timestamp.asc, :conditions=>{'entities.media.0.media_url'=>{:$exists=>true}, 'entities.media.0.sizes.small.h'=>{:$exists=>true}, 'entities.urls.0.expanded_url'=>{'$not'=>/yfrog/}, :timestamp.gte=>1335848461, :timestamp.lte=>1335963661,:block=>{:$exists=>false}}},
+      :mayday=>{:limit=>@page_size, :skip=>skip * @page_size,:order=>:timestamp.asc, :conditions=>{'entities.media.0.media_url'=>{:$exists=>true}, 'entities.media.0.sizes.small.h'=>{:$exists=>true}, 'entities.urls.0.expanded_url'=>{'$not'=>/yfrog/}, :timestamp.gte=>1335848461, :timestamp.lte=>1336163661,:block=>{:$exists=>false}}},
+      :casseroles=>{:limit=>@page_size, :skip=>skip * @page_size,:order=>:timestamp.desc, :conditions=>{'entities.media.0.media_url'=>{:$exists=>true}, 'entities.media.0.sizes.small.h'=>{:$exists=>true}, 'entities.urls.0.expanded_url'=>{'$not'=>/yfrog/}, :timestamp.gte=>1337907661, :timestamp.lte=>1338858061,:block=>{:$exists=>false}}},
       :chicagonato=>{:skip=>skip * @page_size,:limit=>30,:order=>:timestamp.desc, :conditions=>{'entities.media.0.media_url'=>{:$exists=>true},'entities.media.0.sizes.small.h'=>{:$exists=>true},:timestamp.gte=>1337302861, :timestamp.lte=>1380949261,:block=>{:$exists=>false}}}
     }
     @meta = {
       :mayday =>{:page_title =>"Occupy Mosaic - May Day",:description=>"A mosaic of photos that were posted worldwide during the May Day General Strike"},
+      :casseroles =>{:page_title =>"Occupy Mosaic - Quebec and Quebec Solidarity",:description=>"A mosaic of the Quebec #manifencours actions and solidarity actions in NYC"},
       :chicagonato => {:page_title => "Occupy Mosaic - Chicago - NATO Summit", :description=>"A mosaic of photos posted on Twitter during the protests against the NATO summit in Chicago"}
     }
     @meta_info = @meta[@campaign.to_sym]
